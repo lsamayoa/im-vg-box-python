27d9ae8802c5c5286de46ca8b8c9ccad71d3df63
lsamayoa <leonel.e.samayoa@gmail.com>
2014-03-03 15:16:01 -0600

    Ya se genera el zip

--- cookbooks/im-reach/recipes/default.rb
+++ cookbooks/im-reach/recipes/default.rb
@@ -1,2 +1,19 @@
 include_recipe "im-core-python"
 include_recipe "phantomjs::default"
+include_recipe "git"
+
+directory "/Highcharts"
+
+git "/Highcharts/highcharts.com" do
+ repository "https://github.com/highslide-software/highcharts.com.git"
+ reference "master"
+ action :sync
+end
+
+bash "run_highcharts_webserver" do
+  user "root"
+  cwd "/Highcharts/highcharts.com/exporting-server/phantomjs"
+  code <<-EOH
+  phantomjs highcharts-convert.js -host 0.0.0.0 -port 3003
+  EOH
+end
